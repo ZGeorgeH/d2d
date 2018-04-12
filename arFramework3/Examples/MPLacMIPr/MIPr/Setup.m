2a0eb023ced83ab0bb3f90b96779560db6dd71c9
ZGeorgeH <george-92@live.cn>
2018-04-12 11:00:15 +0100

    import in-silico experimental data

--- arFramework3/Examples/MPLacMIPr/MIPr/Setup.m
+++ arFramework3/Examples/MPLacMIPr/MIPr/Setup.m
@@ -2,10 +2,10 @@ close all; clear all; clc;
 % initialize full model
 arInit;
 arLoadModel('MIPr');
+arLoadData('MIPrExp1');
 
 %the error model is used and estimated together with the dynamics
 ar.config.fiterrors = 1;
-
 arCompileAll;
 
 %set up the initial condition to the steady state of the network.
@@ -20,12 +20,13 @@ arSteadyState(model, sourceCondition, targetCondition, -1e7);
 % Cit_fluo[next]=1*Cit_fluo+500
 %arAddEvent(1, 1, 300,'Cit_fluo',1,500);
 
-%print out the values of the parameters
-arPrint;
 %plot the states and observables
 arPlot;
 
-%identifiability test
-%arIdentifiablityTest(false,1,100,false);
+%print out the values of the parameters
+arPrint;
 
+
+%identifiability test
+arIdentifiablityTest(false,0.5,100,true);
 return;
